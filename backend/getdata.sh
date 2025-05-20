cd8e7bbade1463bbcfbb246058f5db79376555c2
EinarBalan <einar.balan@gmail.com>
2025-05-20 11:37:52 -0700

    fix listener memory leak + blank post on batch end bug

--- backend/getdata.sh
+++ backend/getdata.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-curl -L -o ~/Downloads/reddit-submissions-dec-2022-to-feb-2023.zip\
+#!/bin/bash
+curl -L -o ~/development/research/slop/backend/archive.zip\
   https://www.kaggle.com/api/v1/datasets/download/noahpersaud/reddit-submissions-dec-2022-to-feb-2023
 
-unzip ~/Downloads/reddit-submissions-dec-2022-to-feb-2023.zip -d ~/Downloads/reddit-submissions-dec-2022-to-feb-2023
+unzip ~/development/research/slop/backend/archive.zip -d ~/development/research/slop/backend/archive
 
-rm ~/Downloads/reddit-submissions-dec-2022-to-feb-2023.zip
+# rm ~/development/research/slop/backend/archive.zip
 
+# https://www.kaggle.com/datasets/noahpersaud/reddit-submissions-july-2021-to-oct-2022
+# https://www.kaggle.com/datasets/noahpersaud/reddit-submissions-dec-2022-to-feb-2023
--- slop-frontend/src/App.tsx
+++ slop-frontend/src/App.tsx
@@ -5,11 +5,22 @@ import forwardIcon from './assets/forward.svg';
 import backIcon from './assets/back.svg';
 import robotIcon from './assets/robot.svg';
 import dislikeIcon from './assets/dislike.svg';
+import { useState, useEffect } from 'react';
 
-function Feed({ title, content }: { title: string; content: string }) {
+interface Post {
+  title: string;
+  post_id: string;
+  over_18: string;
+  subreddit: string;
+  link_flair_text: string;
+  self_text: string;
+}
+
+function Feed({ title, content, subreddit }: { title: string; content: string; subreddit: string }) {
   return (
     <div id="feed">
       <h1>{title}</h1>
+      <p className="subreddit">r/{subreddit}</p>
       <p>{content}</p>
     </div>
   )
@@ -23,25 +34,59 @@ function InteractionButton({ icon, onClick }: { icon: string, onClick: () => voi
   )
 }
 
-function Controls() {
+function Controls({ nextPost, prevPost }: { nextPost: () => void, prevPost: () => void }) {
   return (
     <div id="controls">
       <InteractionButton icon={likeIcon} onClick={() => console.log('Like')} />
       <InteractionButton icon={commentIcon} onClick={() => console.log('Comment')} />
       <InteractionButton icon={robotIcon} onClick={() => console.log('Robot')} />
       <InteractionButton icon={dislikeIcon} onClick={() => console.log('Dislike')} />
-      <InteractionButton icon={backIcon} onClick={() => console.log('Back')} />
-      <InteractionButton icon={forwardIcon} onClick={() => console.log('Forward')} />
+      <InteractionButton icon={backIcon} onClick={prevPost} />
+      <InteractionButton icon={forwardIcon} onClick={nextPost} />
       </div>
   )
 }
 
 function App() {
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [currentPostIndex, setCurrentPostIndex] = useState(0);
+
+  const nextPost = () => {
+    if (currentPostIndex >= posts.length - 1) { // get next batch
+      fetch('http://localhost:3000/batch')
+        .then(response => response.json())
+        .then(data => {
+          setPosts(data.posts);
+          setCurrentPostIndex(0);
+        });
+    } else {
+      setCurrentPostIndex(currentPostIndex + 1);
+    }
+  }
+
+  const prevPost = () => {
+    if (currentPostIndex > 0) {
+      setCurrentPostIndex(currentPostIndex - 1);
+    }
+  }
+
+  useEffect(() => {
+    fetch('http://localhost:3000/batch')
+      .then(response => response.json())
+      .then(data => {
+        setPosts(data.posts);
+        console.log(data.posts);
+      });
+  }, []);
 
   return (
     <div id ="app">
-      <Feed title="Title" content="content"/>
-      <Controls />
+      <Feed 
+        title={posts[currentPostIndex]?.title} 
+        content={posts[currentPostIndex]?.self_text}
+        subreddit={posts[currentPostIndex]?.subreddit}
+      />
+      <Controls nextPost={nextPost} prevPost={prevPost} />
     </div>
   )
 }
