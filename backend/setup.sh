53c84d8bcedc8ceacd417219a4c86c28c78f29f9
Einar Balan <einar.balan@gmail.com>
2025-09-06 13:33:34 +0000

    update setup w/ database url

--- backend/setup.sh
+++ backend/setup.sh
@@ -1,4 +1,6 @@
 pip install dotenv sqlalchemy psycopg[binary]
 
+export DATABASE_URL="postgresql+psycopg://slop:slop@127.0.0.1:65433/slop?connect_timeout=3&sslmode=disable" 
+
 python ./data/sampleposts.py -n 750 --table posts --out ./data/posts.json
 python ./data/sampleposts.py -n 750 --table humorposts --out ./data/humorposts.json
